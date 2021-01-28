9b70480cc225bd12650a7fd3e332f6a38113b8b6
ulisse-fontanot <ulissef99@gmail.com>
2021-01-28 17:59:45 +0100

    tutto funzionante-da perfezionare

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var root = new Vue({
   el: "#root",
   data:{
+    element: "",
+    eroe: "",
     personaggi:[
       "Aragorn",
       "Gandalf",
@@ -10,11 +12,32 @@ var root = new Vue({
     personaggiEliminati:[]
   },
   methods: {
+    addMain(eroe){
+      (this.eroe.length < 4) ? alert("troppo corto...") : this.personaggi.push(this.eroe);
+    },
     toDelete(index){
       this.personaggiEliminati.push(this.personaggi[index]);
       alert("HAI ELIMINATO: " + this.personaggi[index]);
       this.personaggi.splice(index,1);
       console.log(this.personaggiEliminati);
+    },
+    toMain(index){
+      this.personaggi.push(this.personaggiEliminati[index]);
+      this.personaggiEliminati.splice(index,1);
+      console.log(this.personaggi);
+    },
+    destruct(index){
+      this.personaggiEliminati.splice(index,1);
+    },
+    destructAll(){
+      this.personaggiEliminati.splice(0);
+    },
+    cestino(){
+      this.element == document.getElementById("cestin")
+      if(this.personaggiEliminati.length > 0) {
+        this.element.removeclass("sconto");
+        this.element.addclass("attivo");
+      }
     }
   }
 })
